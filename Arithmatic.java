a9bbcd79211e18752e7280df9c11f0a07c15d421
chiranjeevkajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-02-20 20:42:50 +0530

    Java

---
+++ Arithmatic.java
@@ -0,0 +1,25 @@
+
+// Arithmatic operators in java
+public class Arithmatic {
+    public static void main(String[] args) {
+        int num=20;
+        int num1=30;
+
+        System.out.println(num + num1);
+        // postfix operators
+        num++;
+        System.out.println(num);
+        num1++;
+        ++num;
+        System.out.println(num);
+        ++num1;
+        System.out.println(num1);
+        System.out.println(num1);
+        // postfix operators
+        System.out.println(num++);
+        System.out.println(num);
+        // prefix operators
+        System.out.println(++num);
+    }
+    
+}
