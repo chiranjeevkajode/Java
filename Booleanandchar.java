7eaf183d3919420252ae87cf0554df7b516cc6c3
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-02-23 17:22:37 +0530

    Java

---
+++ Booleanandchar.java
@@ -0,0 +1,8 @@
+public class Booleanandchar {
+    public static void main(String[] args) {
+        char Letter='p';
+        String mValue=Integer.toHexString(Letter);
+        System.out.println(mValue);
+        System.out.println(Letter);
+    }
+}
