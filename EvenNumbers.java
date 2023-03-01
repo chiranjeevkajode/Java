67e7f79df454b193c96ccdabf2c3383c72bfffed
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-03-01 22:35:09 +0530

    Java

---
+++ EvenNumbers.java
@@ -0,0 +1,19 @@
+// even and odd numbers
+
+public class EvenNumbers{
+    public static void main(String[] args) {
+        int i=100;
+        for(i=1;i<=100;i++){
+            // EvenNumbers
+            if (i % 2 ==0) {
+                System.out.println("Number is" + i);
+            }
+            // oddnumbers
+            // for(i=1;i<=100;i++){
+            //     if(i % 2 !=0){
+            //         System.out.println(i);
+            //     }
+            // }
+        }
+    }
+}
