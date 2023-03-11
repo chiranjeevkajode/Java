2980dfc340c00e52618f3e4685af12b3bfe126ad
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-03-11 21:25:41 +0530

    Java

---
+++ floatanddubble.java
@@ -0,0 +1,11 @@
+public class floatanddubble {
+    public static void main(String[] args) {
+        int mint= 22/10;
+        float mFloat=22/7f;
+        double mDouble=22/7d;
+        System.out.println("Int is "+ mint);
+        System.out.println("Float is "+ mFloat);
+        System.out.println("Double is "+ mDouble);
+
+    }
+}
