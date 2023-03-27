55a5e91cab9f3d99297c318877f3c92c27049325
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-03-27 21:14:20 +0530

    Java

---
+++ Randomnumber.java
@@ -0,0 +1,8 @@
+public class Randomnumber{
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            System.out.println((int)(Math.random()*100));
+        }
+        
+    }
+}
