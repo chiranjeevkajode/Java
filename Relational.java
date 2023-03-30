b4dc8017682285754d8ab1b42933843323d1073b
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-03-30 21:48:30 +0530

    Java

---
+++ Relational.java
@@ -0,0 +1,26 @@
+public class Relational {
+    public static void main(String[] args) {
+        int num=12;
+        int num2=61;
+        System.out.println(num==num2);
+        System.out.println(num!=num2);
+        System.out.println(num>=num2);
+        System.out.println(num<=num2);
+        System.out.println(num>num2);
+        System.out.println(num<num2);
+        // System.out.println(num==num2);
+        // System.out.println(num==num2);
+        
+    }
+    
+    // Processs ID 
+    // public class SelectionSort {
+
+
+        // public static void main(String[] args) {
+        //     ProcessHandle processHandle = ProcessHandle.current();
+        //     System.out.println("Process Handle : " + processHandle.pid());
+        // }
+    // }
+    
+}
