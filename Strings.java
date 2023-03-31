10c159def535230b08505a3802433c253e89800b
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-03-31 16:44:33 +0530

    Java

---
+++ Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String[] args) {
+        String You="My name  is Ram ";
+        String no="My surname is Raj";
+        System.out.println(You + no);
+        System.out.println(You.toUpperCase());
+    }
+}
