b347d1c8945e917acd4dc638f94abdc296f48117
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-04-16 19:55:12 +0530

    Java

---
+++ OPPS Java/BankUser.java
@@ -0,0 +1,22 @@
+public class BankUser {
+    public static void main(String[] args) {
+        // BankApp rahul = new BankApp("rahul mahajan");
+
+        // rahul.userAge(25);
+        // rahul.account(1000030);
+        // rahul.getUserinfo();
+
+    //Inheritance
+
+        Inheritance mohan = new Inheritance("mohan");
+        mohan.account(12345);
+        mohan.userTax("nine-thousand");
+        mohan.getUserinfo();
+
+
+
+    }
+
+    
+
+}
